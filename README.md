88df68f033fb1ce01bd5bf2424ace5b3fe78a8fe
Kirill Varlamov <k.v.varlamov@gmail.com>
2025-02-20 17:19:01 +0100

    feat(nodes): run telemetry service with eth nodes

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# eth-p2p-security-research-data
-Ethereum P2P Security Toolkit: Datasets, Analysis, and Research Instruments
+# Ethereum P2P Security Toolkit: Datasets, Analysis, and Research Instruments
+
+## Run Ethereum EN + CN stack with telemetry
+
+Prepare the host
+
+```bash
+openssl rand -hex 32 > /mnt/jwt.hex
+chmod ugo+r /mnt/jwt.hex
+mkdir -p /data/lighthouse
+mkdir -p /data/reth
+mkdir -p /dumps
+chmod ugo+rw /dumps
+chmod -R ugo+rw /data/
+```
+
+Run the stack Execution Node + Consensus Node + Telemetry
+
+```bash
+export DOCKER_HOST=ssh://root@host
+docker compose up --build -d
+```
+
+The telemetry engine will write PCAP files and Peer JSONs to `/dumps` directory.
--- docker_telemetry/entrypoint.py
+++ docker_telemetry/entrypoint.py
@@ -4,7 +4,14 @@ import sys
 import time
 import select
 import os
+import requests
+import json
 from datetime import datetime, timezone
+
+IFACE = os.getenv("IFACE", "eth0")
+RETH = os.getenv("RETH", "http://127.0.0.1:8545")
+LIGHTHOUSE = os.getenv("LIGHTHOUSE", "http://127.0.0.1:5052")
+
 
 subprocess.run(["sysctl", "-w", "net.ipv4.icmp_echo_ignore_broadcasts=1"])
 subprocess.run(["sysctl", "-w", "net.ipv4.icmp_ignore_bogus_error_responses=1"])
@@ -16,15 +23,15 @@ subprocess.run(["sysctl", "-w", "net.ipv4.ip_forward=1"])
 subprocess.run(["iptables", "-t", "nat", "-A", "PREROUTING", "-i", "eth0", "-p", "tcp", "--dport", "80", "-j", "DNAT", "--to-destination", "1.1.1.1:80"])
 subprocess.run(["iptables", "-t", "nat", "-A", "POSTROUTING", "-o", "eth0", "-j", "MASQUERADE"])
 
-DUMPS_STORAGE = "/tmp"
+DUMPS_STORAGE = "/dumps"
 START_TIME = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H-%M-%S.%f')[:-3]
 
 # List of commands to run as background processes
 
 commands = [
-    ["tcpdump", "-i", "eth0", "-n", "-vvv", "-s", "80", "-w", f"{DUMPS_STORAGE}/{START_TIME}-tcpdump.pcap", "-C", "100"],
-    ["tcpdump", "-U", "-i", "lo", "-n", "-vvv", "-X", "-w", f"{DUMPS_STORAGE}/{START_TIME}-netflow.pcap", "udp", "port", "2055"],
-    ["softflowd", "-i", "eth0", "-n", "127.0.0.1:2055", "-v", "10", "-P", "udp", "-D"],
+    ["tcpdump", "-i", IFACE, "-n", "-vvv", "-s", "80", "-C", "10", "-w", f"{DUMPS_STORAGE}/{START_TIME}-tcpdump.pcap", "not", "port", "22"],
+#    ["tcpdump", "-U", "-i", "lo", "-n", "-vvv", "-X", "-w", f"{DUMPS_STORAGE}/{START_TIME}-netflow.pcap", "udp", "port", "2055"],
+#    ["softflowd", "-i", "eth0", "-n", "127.0.0.1:2055", "-v", "10", "-P", "udp", "-D"],
 ]
 
 processes = []
@@ -62,6 +69,42 @@ def signal_handler(sig, frame):
     stop_processes()
     sys.exit(0)
 
+def dump_lighthouse_api_peers():
+    LIGHTHOUSE_PEERS_HTTP_URI = f"{LIGHTHOUSE}/lighthouse/peers/"
+    try:
+        response = requests.get(LIGHTHOUSE_PEERS_HTTP_URI)
+        response.raise_for_status()
+        iso_datitime = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H-%M-%S.%f')[:-3]
+        file_path = f"{DUMPS_STORAGE}/{iso_datitime}-cons-peers.json"
+        with open(file_path, 'w') as file:
+            file.write(response.text)
+        peers_total = len(json.loads(response.text))
+        print(f"Consensus peers are fetched and dumped to {file_path}. Total:{peers_total}")
+    except requests.RequestException as e:
+        print(f"Failed to fetch peers from ETH2 {e}")
+
+def dump_execution_api_peers():
+    url = RETH
+    headers = {"Content-Type": "application/json"}
+    payload = {
+        "jsonrpc": "2.0",
+        "method": "admin_peers",
+        "params": [],
+        "id": 1
+    }
+    try:
+        response = requests.post(url, headers=headers, data=json.dumps(payload))
+        response.raise_for_status()
+        iso_datitime = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H-%M-%S.%f')[:-3]
+        file_path = f"{DUMPS_STORAGE}/{iso_datitime}-exec-peers.json"
+        peers = response.json().get("result", [])
+        with open(file_path, 'w') as file:
+            file.write(json.dumps(peers))
+        peers_total = len(peers)
+        print(f"Execution peers are fetched and dumped to {file_path}. Total:{peers_total}")
+    except requests.RequestException as e:
+        print(f"Failed to fetch peers from ETH1: {e}")
+
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
@@ -82,9 +125,9 @@ if __name__ == "__main__":
                 line = process.stderr.readline()
                 if line:
                     print(line, flush=True)
-                else:
-                    time.sleep(0.1)
-            time.sleep(0.1)  # Reduce CPU usage
+            dump_lighthouse_api_peers()
+            dump_execution_api_peers()
+            time.sleep(10)  # Reduce CPU usage
     except KeyboardInterrupt:
         pass
     finally:
